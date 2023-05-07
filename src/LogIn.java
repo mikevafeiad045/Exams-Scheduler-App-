ea77a9460f3bdf92e05fd8f709f85912fb27ded7
mikevafeiad045 <mikevaf03@gmail.com>
2023-05-07 20:29:20 +0300

    LogIn Class method
    Added method for verifying user input (password).

--- src/LogIn.java
+++ src/LogIn.java
@@ -5,5 +5,14 @@ public class LogIn {
 	{
 		
 	}
-
+	
+	public int checkUserProperty(String password) {
+		if(password.equals("$3cReT4rY")) {
+			return 1;
+		}else if(password.equals("pR0!3$$oR")) {
+			return 2;
+		}else {//WRONG PASSWORD
+			return 0;
+		}
+	}
 }
