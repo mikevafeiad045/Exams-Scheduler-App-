7968e781f6a2038c9b5b1d3058d193bd1e4fc764
harin <harinamakri123@gmail.com>
2023-05-10 16:16:27 +0300

    creating method calcRemainingStudents

--- src/ExamScheduler.java
+++ src/ExamScheduler.java
@@ -2,6 +2,22 @@
 public class ExamScheduler extends Secretary{
 	
 	private Class[][]availability;
+
+	
+	 public ExamScheduler(int period, int capacityAud, int capacityAmph, int numberOfAud, int numberOfAmph,
+			Class[][] availability) {
+		super(period, capacityAud, capacityAmph, numberOfAud, numberOfAmph);
+		this.availability = availability;
+	}
+
+
+
+	public void calcRemainingStudents(Course c) {
+		
+		
+		
+		 
+	}
 	
 	
 }
