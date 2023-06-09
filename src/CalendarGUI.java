e7443de8aed214595671fd0e28d3023a73be3204
PapadopoulouDimitra <132615454+PapadopoulouDimitra@users.noreply.github.com>
2023-06-09 21:17:44 +0300

    Calendar progress #2

--- src/CalendarGUI.java
+++ src/CalendarGUI.java
@@ -1,5 +1,6 @@
 import java.awt.BorderLayout;
 import java.util.ArrayList;
+import java.util.Map;
 
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
@@ -7,12 +8,14 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.ListModel;
 
 
 public class CalendarGUI extends JFrame{
 
-	private DefaultListModel<ExamDate> model;
+	private DefaultListModel<String> model;
 	private ArrayList<ExamDate> exams =new ArrayList<>();
+	private JList<String> list;
 	
 	public CalendarGUI(ExamScheduler ES) {
 		
@@ -24,34 +27,54 @@ public class CalendarGUI extends JFrame{
 		JPanel panel = new JPanel();
 		getContentPane().add(panel);
 		
-		JList<ExamDate> list = new JList<ExamDate>();
+		list = new JList<String>();
 		list.setBounds(0, 0, 484, 461);
 		getContentPane().add(list);
 		list.setFont(list.getFont().deriveFont(18.0f));
 		list.setFixedCellHeight(44);
 		
-		/*DefaultListModel model = new DefaultListModel();
-		model.addElement("sifalera");
-		model.addElement("sam");
-		list.setModel(model);*/
-
-	
-		this.model = new DefaultListModel<>();
-		
-		list.setModel(model);
-		for(ExamDate d: exams) {
-			Course[] e1 = d.getZone();
-			
-		
-			
-			
-			model.addElement(d);
-		}
-		
 		JScrollPane scrollPane = new JScrollPane(list);
 		getContentPane().add(scrollPane, BorderLayout.CENTER);
 		revalidate();
 		list.setVisible(true);
+		
+		this.model = new DefaultListModel<>();
+		
+		list.setModel(model);
+		for(ExamDate d: exams) {
+			String examDayElem = d.day; 
+			Course[] coursesZone= d.getZone();
+			String courseNameElem;
+			ArrayList<String> profs = new ArrayList<>();
+			ArrayList<Room> rooms = new ArrayList<>();
+			String combinedElements;
+			int i;
+			for(i=0;i<6;i++) {
+				if(coursesZone[i]!=null) {
+					courseNameElem = coursesZone[i].getCourseName();
+					profs = coursesZone[i].getProfessorsList();
+					rooms = coursesZone[i].getSelectedRoomsList();
+					StringBuilder sb = new StringBuilder();
+					for(int j=0; j<profs.size();j++) {
+						sb.append(profs.get(j));
+						if(j<profs.size()-1) {
+							sb.append(", ");
+							}
+						}
+					StringBuilder sb2 = new StringBuilder();
+					for(int j=0; j<rooms.size();j++) {
+						sb2.append(rooms.get(j).getRoomName());
+						if(j<rooms.size()-1) {
+							sb2.append(", ");
+							
+							}
+						}
+					combinedElements = examDayElem+courseNameElem+sb.toString()+sb2.toString();
+					model.addElement(combinedElements);
+					}
+			}
+			
+		}
 		
 		addWindowListener(new ProgramTerminated(ES));
 		
